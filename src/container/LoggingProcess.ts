cd0d255779b7cd557f76cfcd1881229ecff54be4
Dmitriy Krayniy <krdima92@gmail.com>
2018-05-15 18:04:19 +0300

    added new build structure

--- src/container/LoggingProcess.ts
+++ src/container/LoggingProcess.ts
@@ -1,5 +1,7 @@
 import { IReferences } from 'pip-services-commons-node';
 import { ProcessContainer } from 'pip-services-container-node';
+import { DefaultNetFactory } from 'pip-services-net-node';
+import { DefaultOssFactory } from 'pip-services-oss-node';
 
 import { LoggingServiceFactory } from '../build/LoggingServiceFactory';
 
@@ -8,6 +10,8 @@ export class LoggingProcess extends ProcessContainer {
     public constructor() {
         super("logging", "Trace logging microservice");
         this._factories.add(new LoggingServiceFactory);
+        this._factories.add(new DefaultNetFactory);
+        this._factories.add(new DefaultOssFactory);
     }
 
 }
